afd4033a8a87ad3bca1b7db3ac466a9fb9b82224
Fabian Isensee <f.isensee@dkfz.de>
2026-06-19 08:28:49 +0200

    bump to 2.3.0, version is now defined in pyproject.toml

--- src/napari_nninteractive/__init__.py
+++ src/napari_nninteractive/__init__.py
@@ -1,4 +1,10 @@
-__version__ = "2.2.0"
+from importlib.metadata import PackageNotFoundError, version
+
 from .widget_main import nnInteractiveWidget
+
+try:
+    __version__ = version("napari-nninteractive")
+except PackageNotFoundError:
+    __version__ = "unknown"
 
 __all__ = ("nnInteractiveWidget",)
